8c0f2215cfc18cfea7fadf80606d0920f40b2eba
Mathew4202 <w0461513@campus.nscc.ca>
2024-09-26 17:10:41 -0300

    Update A1-P1-Hipsters.py

--- A1-P1-Hipsters.py
+++ A1-P1-Hipsters.py
@@ -9,18 +9,23 @@ def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
     #Declare variables
+    #In this section i declare my variables for the delivery fee and sales tax
     deliveryFee = 15
     salesTax = 0.14
     # Ask for customers name
+    #in this section we prompt user to input the customer they want to calculate for
     customersName = input("Enter the customer's name:")
 
     # Ask user for the distance in kilometers driven to deliver
+    #In this section we ask the user to input the distance taken to deliver the record to the customer and then we use the float to switch it from a string to a float
     distance = float(input("Enter the distance in kilometers for delivery:"))
 
     # Ask user to input the cost of any records purchased by the customer
+     #In this section we ask the user to input the cost of records of the customer and then we use the float to switch it from a string to a float
     costofRecords = float(input("Enter the cost of records purchased:"))
 
     # Perform calculations
+    # since all values inputed by the user has been changed to floate we then do the neccessary calculations
     deliveryCost = distance * deliveryFee
     purchaseCost = (salesTax * costofRecords) + costofRecords 
     totalCost = purchaseCost + deliveryCost
@@ -29,6 +34,7 @@ def main():
     print("Purchase summary for", customersName)
 
     # Print selivery, purchase and total cost
+    # make sure to change is to print in 2 decimal places as thats the required formad for currency
     print(f"Delivery Cost: ${deliveryCost:.2f}")
     print(f"Purchase Cost: ${purchaseCost:.2f}")
     print(f"Total Cost: ${totalCost:.2f}")
